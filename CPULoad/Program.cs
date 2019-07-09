8ea003d3fd3f8e8cbdb48657375a3662faf47c23
Ahmet DAGTAS <a.dagtas@gmail.com>
2019-07-09 23:38:27 +0300

    Initial commit
    Completion of program

---
+++ CPULoad/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CPULoad
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 50; i++)
+			{
+				Test t = new Test();
+				t.CreateThread();
+			}
+		}
+
+		private class Test
+		{
+			public void CreateThread()
+			{
+				new Thread(Write) { }.Start();
+			}
+
+			private void Write()
+			{
+				for (long i = 0; i < long.MaxValue; i++)
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
